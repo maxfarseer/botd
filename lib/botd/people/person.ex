47fc11b2556876be36452f250bc3293a66cfbf60
maxfarseer <maxfarseer@gmail.com>
2025-04-09 22:27:40 +0200

    add validation: date of death can not be in the future

--- lib/botd/people/person.ex
+++ lib/botd/people/person.ex
@@ -27,9 +27,20 @@ defmodule Botd.People.Person do
       :description
     ])
     |> validate_required([:name, :death_date])
+    |> validate_death_date_not_in_future()
     |> unique_constraint([:name, :death_date],
       name: :people_name_death_date_index,
       message: "A person with this name and death date already exists"
     )
+  end
+
+  defp validate_death_date_not_in_future(changeset) do
+    validate_change(changeset, :death_date, fn :death_date, death_date ->
+      if Date.compare(death_date, Date.utc_today()) == :gt do
+        [death_date: "cannot be in the future"]
+      else
+        []
+      end
+    end)
   end
 end

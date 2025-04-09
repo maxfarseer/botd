01db659350a38b1c98417492dc9bfbbf8fb7d334
maxfarseer <maxfarseer@gmail.com>
2025-04-09 22:19:23 +0200

    add form to create new person

--- lib/botd_web/controllers/person_controller.ex
+++ lib/botd_web/controllers/person_controller.ex
@@ -1,10 +1,28 @@
 defmodule BotdWeb.PersonController do
   use BotdWeb, :controller
   alias Botd.People
+  alias Botd.People.Person
 
   def index(conn, _params) do
     people = People.list_people()
     render(conn, :index, people: people)
+  end
+
+  def new(conn, _params) do
+    changeset = People.change_person(%Person{})
+    render(conn, :new, changeset: changeset)
+  end
+
+  def create(conn, %{"person" => person_params}) do
+    case People.create_person(person_params) do
+      {:ok, person} ->
+        conn
+        |> put_flash(:info, "Person created successfully.")
+        |> redirect(to: ~p"/people/#{person}")
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, :new, changeset: changeset)
+    end
   end
 
   def show(conn, %{"id" => id}) do
--- lib/botd_web/controllers/person_html.ex
+++ lib/botd_web/controllers/person_html.ex
@@ -2,4 +2,32 @@ defmodule BotdWeb.PersonHTML do
   use BotdWeb, :html
 
   embed_templates "person_html/*"
+
+  @doc """
+  Renders a person form.
+  """
+  attr :changeset, Ecto.Changeset, required: true
+  attr :action, :string, required: true
+
+  def person_form(assigns) do
+    ~H"""
+    <.simple_form :let={f} for={@changeset} action={@action}>
+      <.error :if={@changeset.action}>
+        Oops, something went wrong! Please check the errors below.
+      </.error>
+
+      <.input field={f[:name]} type="text" label="Name" required />
+      <.input field={f[:nickname]} type="text" label="Nickname" />
+      <.input field={f[:birth_date]} type="date" label="Birth date" />
+      <.input field={f[:death_date]} type="date" label="Death date" required />
+      <.input field={f[:place]} type="text" label="Place" />
+      <.input field={f[:cause_of_death]} type="textarea" label="Cause of death" />
+      <.input field={f[:description]} type="textarea" label="Description" />
+
+      <:actions>
+        <.button>Save Person</.button>
+      </:actions>
+    </.simple_form>
+    """
+  end
 end

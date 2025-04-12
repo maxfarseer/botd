6ed5ef9afc9d19d9c2c0075986066d4c47ad53ab
Max P <maxfarseer@gmail.com>
2025-04-12 15:46:25 +0200

    Add: hide action button for non registred users (#11)
    * hide action button for non registred users
    * add test where assert for action icons

---
+++ test/botd_web/controllers/person_html_test.exs
@@ -0,0 +1,69 @@
+defmodule BotdWeb.PersonHTMLTest do
+  use BotdWeb.ConnCase, async: true
+
+  alias Botd.People
+  alias Botd.Repo
+  alias Botd.Users.User
+
+  setup do
+    person_attrs = %{
+      name: "Test Person",
+      death_date: ~D[2020-01-01],
+      place: "Test City"
+    }
+
+    {:ok, person} = People.create_person(person_attrs)
+
+    user_params = %{
+      email: "test@example.com",
+      password: "secret1234",
+      password_confirmation: "secret1234"
+    }
+
+    {:ok, user} = %User{} |> User.changeset(user_params) |> Repo.insert()
+
+    %{person: person, user: user}
+  end
+
+  describe "Person show.html" do
+    test "hide Edit and Delete buttons for authenticated users", %{
+      conn: conn,
+      person: person
+    } do
+      conn = get(conn, ~p"/people/#{person}")
+
+      assert html_response(conn, 200) =~ person.name
+
+      html = html_response(conn, 200)
+      {:ok, document} = Floki.parse_document(html)
+
+      edit_button = Floki.find(document, "a[href*='/edit']")
+      assert edit_button == []
+
+      delete_button = Floki.find(document, "a[data-method='delete']")
+      assert delete_button == []
+    end
+
+    test "shows Edit and Delete buttons for authenticated users", %{
+      conn: conn,
+      person: person,
+      user: user
+    } do
+      conn =
+        conn
+        |> Pow.Plug.assign_current_user(user, otp_app: :my_app)
+        |> get(~p"/people/#{person}")
+
+      assert html_response(conn, 200) =~ person.name
+
+      html = html_response(conn, 200)
+      {:ok, document} = Floki.parse_document(html)
+
+      edit_button = Floki.find(document, "a[href$='/edit']")
+      assert edit_button != []
+
+      delete_button = Floki.find(document, "a[data-confirm]")
+      assert delete_button != []
+    end
+  end
+end

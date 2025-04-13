b17969891131ce75b108d16daba402da017c4b97
maxfarseer <maxfarseer@gmail.com>
2025-04-13 07:53:46 +0200

    update config

--- config/config.exs
+++ config/config.exs
@@ -71,3 +71,18 @@ config :botd, :pow,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+if Mix.env() == :dev do
+  config :git_hooks,
+    auto_install: true,
+    verbose: true,
+    hooks: [
+      pre_push: [
+        tasks: [
+          {:cmd, "mix format --check-formatted"},
+          {:cmd, "mix credo --strict"},
+          {:cmd, "mix test"}
+        ]
+      ]
+    ]
+end

38fc200a4b59ae0c7fb1f14982afaf45c478dd4c
maxfarseer <maxfarseer@gmail.com>
2025-04-13 07:53:13 +0200

    add deps

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule Botd.MixProject do
       {:bandit, "~> 1.5"},
       # Added
       {:pow, "~> 1.0.39"},
-      {:floki, ">= 0.37.1", only: :test}
+      {:floki, ">= 0.37.1", only: :test},
+      {:git_hooks, "~> 0.8.0", only: [:dev], runtime: false}
     ]
   end
 

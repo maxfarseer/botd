31fa200be190aec69df207602ae035c1ad6d7afc
maxfarseer <maxfarseer@gmail.com>
2025-04-13 07:58:06 +0200

    adding credo deps

--- mix.exs
+++ mix.exs
@@ -60,7 +60,8 @@ defmodule Botd.MixProject do
       # Added
       {:pow, "~> 1.0.39"},
       {:floki, ">= 0.37.1", only: :test},
-      {:git_hooks, "~> 0.8.0", only: [:dev], runtime: false}
+      {:git_hooks, "~> 0.8.0", only: [:dev], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 

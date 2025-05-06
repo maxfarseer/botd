8e557b69eeac24c9778d22ae7989cc9fb1863087
maxfarseer <maxfarseer@gmail.com>
2025-05-06 12:12:02 +0200

    it shows the message from telegram bot, withtout loop yet

--- lib/botd_web/controllers/telegram_controller.ex
+++ lib/botd_web/controllers/telegram_controller.ex
@@ -5,10 +5,36 @@ defmodule BotdWeb.TelegramController do
                     :telegramToken
                   ]
 
-  def playground(conn, _params) do
-    {:ok, %{"first_name" => first_name, "username" => username}} =
-      Telegram.Api.request(@telegram_token, "getMe")
+  # def playground(conn, _params) do
+  #   {:ok, %{"first_name" => first_name, "username" => username}} =
+  #     Telegram.Api.request(@telegram_token, "getMe")
 
-    render(conn, :playground, info: %{first_name: first_name, username: username})
+  #   render(conn, :playground, info: %{first_name: first_name, username: username})
+  # end
+
+  def playground(conn, _params) do
+    case Telegram.Api.request(@telegram_token, "getUpdates", offset: -1, timeout: 30) do
+      {:ok, message} ->
+        IO.inspect(message)
+
+        first_name =
+          message
+          |> List.first()
+          |> Map.get("message")
+          |> Map.get("from")
+          |> Map.get("first_name")
+
+        text =
+          message
+          |> List.first()
+          |> Map.get("message")
+          |> Map.get("text")
+
+        render(conn, :playground, info: %{first_name: first_name, text: text})
+
+      {:error, reason} ->
+        IO.inspect(reason, label: "Error fetching updates from Telegram bot")
+        render(conn, :playground, info: %{first_name: "Unknown", text: "Error fetching updates"})
+    end
   end
 end

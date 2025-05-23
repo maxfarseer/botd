66721e26ee340b4bc7edb278b7d9f32d93d5dcc1
Max P <maxfarseer@gmail.com>
2025-05-23 11:55:25 +0200

    markup for person list

---
+++ lib/botd_web/components/person_card.ex
@@ -0,0 +1,33 @@
+defmodule BotdWeb.PersonCard do
+  use Phoenix.Component
+  import BotdWeb.CoreComponents
+
+  attr :person, :map, required: true
+
+  def person_card(assigns) do
+    ~H"""
+    <div class="flex items-center bg-white rounded-lg shadow p-4 mb-4">
+      <%= if @person.photo_url do %>
+        <img
+          src={@person.photo_url}
+          alt={@person.name}
+          class="w-20 h-20 object-cover rounded-full border mr-6"
+        />
+      <% else %>
+        <.icon name="hero-camera" class="w-20 h-20 text-gray-300 mr-6" />
+      <% end %>
+      <div class="flex-1">
+        <div class="text-xl"><%= @person.name %></div>
+        <div class="text-gray-500 text-sm mb-2">
+          <%= if @person.death_date do %>
+            ✝ <%= @person.death_date %>
+          <% end %>
+        </div>
+        <.link href={"/people/#{@person.id}"} class="text-blue-600 hover:underline">
+          Read more
+        </.link>
+      </div>
+    </div>
+    """
+  end
+end

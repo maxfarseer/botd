a8bfd1f969cfa5100223d4f4f8b4060d79ad742b
Max P <maxfarseer@gmail.com>
2025-10-18 12:42:14 +0200

    update mix deps, tidewave require new veriosn of phoenix live

--- lib/botd_web/endpoint.ex
+++ lib/botd_web/endpoint.ex
@@ -25,6 +25,11 @@ defmodule BotdWeb.Endpoint do
     gzip: false,
     only: BotdWeb.static_paths()
 
+  # Tidewave AI
+  if Code.ensure_loaded?(Tidewave) do
+    plug Tidewave
+  end
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
